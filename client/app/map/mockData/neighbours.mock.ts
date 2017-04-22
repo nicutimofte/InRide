06d85e65fb27a7e7f1a69bad1e568ec11031d2fa
Nicu <timofte.alexandru05@yahoo.com>
2017-04-22 15:18:36 +0300

    added map with origin + destinations inputs and showing directions

---
+++ client/app/map/mockData/neighbours.mock.ts
@@ -0,0 +1,19 @@
+
+// type Route = {
+//     id: number,
+//     lat: number,
+//     lng:number
+// };
+//
+// let routes: Route[] = [
+//     { "id": 0, "lat": "", lng: },
+//     { "id": 1, "lat": "", lng },
+//     { "id": 2, "lat": "", lng: }
+// ];
+
+ let neighbours= [
+    {latitude: 46.7700713,longitude: 23.590370300000018},
+    {latitude: 46.7695434,longitude:23.7131124},
+    {latitude: 46.7719576,longitude: 23.627796699999976}
+];
+module.exports.neighbours = neighbours;
